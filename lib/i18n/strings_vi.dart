d6da9db7bcc31ed49185a38717e0b7c9509ee634
quyendk98 <42664009+quyendk98@users.noreply.github.com>
2020-10-22 13:02:53 +0700

    initial commit

--- lib/i18n/strings_vi.dart
+++ lib/i18n/strings_vi.dart
@@ -17,18 +17,18 @@ class _StringsVI extends _StringsI18n {
   @override
   List<String> getMonths() {
     return [
-      "Tháng 1",
-      "Tháng 2",
-      "Tháng 3",
-      "Tháng 4",
-      "Tháng 5",
-      "Tháng 6",
-      "Tháng 7",
-      "Tháng 8",
-      "Tháng 9",
-      "Tháng 10",
-      "Tháng 11",
-      "Tháng 12"
+      "Th 1",
+      "Th 2",
+      "Th 3",
+      "Th 4",
+      "Th 5",
+      "Th 6",
+      "Th 7",
+      "Th 8",
+      "Th 9",
+      "Th 10",
+      "Th 11",
+      "Th 12"
     ];
   }
 
